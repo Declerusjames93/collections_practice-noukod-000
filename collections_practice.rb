bdb6fc06da408ab524c0a25df14a3f6ad1c20924
James Declerus <james.declerus@esih.edu>
2019-04-16 19:01:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,73 @@
+
 def sort_array_asc(array)
-i == 0
-array1 = []
-  array.sort {|a, b|
-while i < array.length
-  if a < b
-  array1 << a
-else a == b
-array1 << b
+  array.sort
+end
+
+sort_array_asc([25, 7, 1]) #[1,7,25]
+
+def sort_array_desc(array)
+  array.sort do | left, right|
+    right <=> left
   end
 end
-  }
+
+sort_array_desc([25, 7, 14]) #[25, 14, 7]
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+kesha_maker(["blake", "ashley", "scott"]) #["bl$ke", "as$ley", "sc$tt"]
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+
+end
+
+find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) 
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+end 
+  
+end
+
+sum_array([11,4,7,8,9,100,134]) #273
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
